e8f8aeeb83a3aaf0226c839fc79a6522377057bc
Niranjan1478 <niranjan.chennaka444@gmail.com>
2022-11-14 14:27:08 +0530

    Gusser Game
    Add code for Gusser Game

---
+++ Guesser Game/src/com/game/Guesser.java
@@ -0,0 +1,26 @@
+package com.game;
+
+import java.util.Scanner;
+
+public class Guesser
+{
+	
+	Scanner sc=new Scanner(System.in);
+	GussersFields gf1=new GussersFields() ; int num;
+	int GusserNumber()
+	{
+		Guesser gg=new Guesser();
+		System.out.println("Gusser Kindly Enter your number: ");
+		gf1.setGuesserNum(sc.nextInt());
+		int num=gf1.getGuesserNum();
+		
+		if(num<0 || num>10)
+		{
+			System.out.println("Kindly Enter your number: in between (0 to 11) ");
+			GusserNumber();
+		}
+		return gf1.getGuesserNum();
+
+	}
+
+}
---
+++ Guesser Game/src/com/game/GusserMain.java
@@ -0,0 +1,19 @@
+package com.game;
+
+public class GusserMain 
+{
+	
+	static Umpire um=new Umpire();
+	public static void main(String[] args)
+	{
+		System.out.println("WELCOME TO GUSSER GAME :)");
+		um.collectNumFromGusser();
+		GusserMain1();
+	}
+	static void GusserMain1()
+	{
+		um.collectNumFromPlayers();
+		um.compare();
+	}
+
+}
